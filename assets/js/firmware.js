2a96af6d3fa5aa92342fb3397ce809225553dd7b
yshalsager <ysh-alsager@hotmail.com>
2019-11-22 13:29:04 +0200

    js: firmware: archive: improve android version matching for fastboot this fixes broken page because of android 10 fastboot links
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- assets/js/firmware.js
+++ assets/js/firmware.js
@@ -507,7 +507,7 @@ function loadMiuiArchive(device) {
                                 var android = filename.split('_').slice(-1).join().split('.zip')[0];
                             }
                             else {
-                                var android = filename.match(/_[0-9].[0-9]_/)[0].split('_')[1];
+                                var android = filename.match(/_[0-9]+\.[0-9]_/)[0].split('_')[1];
                             }
                             var region;
                             if (full_codename.includes('eea_global')) {
